aee2f13e8b6155851939eb766ffe510cd5dd5922
mohiuddinmohi <mohiuddin0240@gmail.com>
2021-05-05 23:08:27 +0600

    html and css file write

---
+++ script.js
@@ -0,0 +1,29 @@
+const sounds = ['bubble', 'trumpet', 'twincle']
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button')
+
+  btn.classList.add('btn')
+
+  // stopSongs()
+
+  btn.innerText = sound
+
+  btn.addEventListener('click', () => {
+    stopSongs()
+
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime = 0;
+  })
+
+}
